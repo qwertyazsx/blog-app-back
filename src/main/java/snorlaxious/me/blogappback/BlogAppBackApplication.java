2adead6eca05ba42d18887eab346c9a8b54fc2ec
Junghyun Park <qwertyazsxc@gmail.com>
2020-06-16 18:48:19 +0900

    first commit

---
+++ src/main/java/snorlaxious/me/blogappback/BlogAppBackApplication.java
@@ -0,0 +1,11 @@
+package snorlaxious.me.blogappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppBackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAppBackApplication.class, args);
+    }
+}
---
+++ src/test/java/snorlaxious/me/blogappback/BlogAppBackApplicationTests.java
@@ -0,0 +1,11 @@
+package snorlaxious.me.blogappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppBackApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
